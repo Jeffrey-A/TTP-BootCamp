c3c823a4f9fff6a9673ea23def9c96a64021b0a7
Jeff A <almanzarortizjeffrey@gmail.com>
2019-05-30 20:41:51 -0400

    program completed

---
+++ Assignment #2/myFilter.js
@@ -0,0 +1,22 @@
+const myFilter = (arr, callback) => {
+    let newArr = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(callback(arr[i])){
+
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+
+};
+
+let x = [1,2,3,4];
+
+console.log("JS Filter");
+let z = x.filter( (item ) => {return item < 3})
+console.log(z);
+
+console.log("myFilter");
+let y = myFilter(x,  (item ) => {return item < 3})
+console.log(y);
